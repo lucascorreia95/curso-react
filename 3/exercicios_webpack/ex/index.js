b2344c7814504bfeeea988c33df63692b60c1436
Lucas Correia <lucascorreia-.-@live.com>
2018-09-15 01:42:11 -0300

    secao 2 webpack

---
+++ 3/exercicios_webpack/ex/index.js
@@ -0,0 +1,4 @@
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa ('Lucas Correia')
+console.log(pessoa.toString())
---
+++ exercicios_webpack/ex/index.js
@@ -0,0 +1,8 @@
+import 'react'
+import './estilo.css'
+
+export default props => (
+    <h1>Teste html</h1>
+)
+
+console.log("Tudo certo!")
