5732d30c4734b88f6ad159b2ca9f64f5889e3d43
Lucas Correia <lucascorreia-.-@live.com>
2018-09-24 22:05:20 -0300

    comecando com redux

---
+++ exercicios_redux/ex1/field.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue } from './fieldActions';
+
+class Field extends Component {
+    render() {
+        return(
+            <div>
+                <label>{this.props.value}</label><br />
+                <input onChange={this.props.changeValue} 
+                value={this.props.value}/>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        value: state.Field.value
+    }
+}
+
+function mapDispathToProps(dispatch){
+    return bindActionCreators({ changeValue }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(Field)
