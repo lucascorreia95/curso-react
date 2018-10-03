0963569af037051bb3b64c3eef7a13797ced24e4
Lucas Correia <lucascorreia-.-@live.com>
2018-10-02 21:00:35 -0300

    lista e inserir ciclos

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -5,6 +5,7 @@ import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
 import Routes from './routes'
+import Messeges from '../common/msg/messages'
 
 export default props => (
     <div className="wrapper">
@@ -14,5 +15,6 @@ export default props => (
             <Routes />
         </div>
         <Footer />
+        <Messeges />
     </div>
 )
